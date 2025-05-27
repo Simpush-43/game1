6d99582646d4e3d30daa3f2259bf4a04a0a1c82f
Simpush-43 <tpsingh02101966@gmail.com>
2025-05-27 21:35:46 +0530

    more chanegsss

--- src/Gamefolder/Socket.jsx
+++ src/Gamefolder/Socket.jsx
@@ -1,7 +1,7 @@
 // Socket.js
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL,{
+const socket = io('wss://game1-production.up.railway.app',{
    transports: ['websocket'],
   withCredentials:true
 }); // Use your backend URL & port
