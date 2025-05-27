ad784da1981258a59eec74f4838e9719b0e133d5
Simpush-43 <tpsingh02101966@gmail.com>
2025-05-27 21:08:29 +0530

    Some change

--- src/Gamefolder/Socket.jsx
+++ src/Gamefolder/Socket.jsx
@@ -2,7 +2,6 @@
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL,{
-   transports: ['websocket'],
   withCredentials:true
 }); // Use your backend URL & port
 
